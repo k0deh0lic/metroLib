06c8cd84e599bcab7b042bfee12ce3559a97834f
kodeholic <78581526+k0deh0lic@users.noreply.github.com>
2021-05-21 21:19:23 +0900

    대규모 수정
    5호선 강일역 누락 문제를 수정(stations.json)
    많은 코드 수정
    readme.md를 코드에 맞게 수정

--- README.md
+++ README.md
@@ -1,7 +1,6 @@
 # metroLib
 metroLib는 대한민국 수도권 전철의 역별 열차 실시간 운행정보를 조회할 수 있는 API 라이브러리입니다.   
 서울시 API(이하 TOPIS API)와 달리 수인·분당선 실시간 운행정보도 가져올 수 있습니다.   
-또한 편성번호 조회도 가능하여 이를 통해 똥차 거르기(...), 자전거 거치대가 설치된 열차를 확인할 수 있습니다. 자전거거치대가 설치된 편성에 대해서는 haveBicycleRack.md를 참조하시기 바랍니다.   
 
 ## 지원하는 노선
 1호선   
@@ -27,7 +26,7 @@ PHP 7.4 이상
 require_once()로 metroLib.php를 참조한 후, metroLib::getInstance()로 객체를 가져옵니다.   
 
 ### 메서드
-getDataByStation(string $line_code, string $station_code) : ?array   
+getDataByStation(string $station_code) : ?array   
 특정 노선의 특정 역 기준으로 열차 목록을 가져옵니다.   
 기준 역 ±4역 범위 내의 열차 운행정보만을 가져올 수 있습니다.   
    
@@ -35,7 +34,7 @@ getDataByLine(string $line_code) : ?array
 특정 노선의 전체 열차 운행정보를 가져옵니다.   
 
 ### 파라메터 설명
-line_code: 노선 코드   
+line_code: 노선 코드
 station_code: 역 코드   
 
 #### 노선 코드
@@ -67,17 +66,14 @@ trn_sts: 열차 상태 코드 (integer)
 line: 노선 코드 (string)   
 is_exp: 급행 여부 (boolean)   
 trn_dir: 열차 운행 방향 (integer)   
-org_stn_nm: 시발역명 (?string)   
-org_stn_cd: 시발역 코드 (?string)   
-dst_stn_nm: 종착역명 (?string)   
-dst_stn_nm: 종착역 코드 (?string)   
 
 #### 열차 상태 코드
 0: 대기   
 1: 진입   
 2: 도착   
-3: 출발(운행중)   
-4: 종착   
+3: 출발   
+4: 운행중
+5: 종착   
 
 #### 열차 운행 방향
 U: 상행   
@@ -99,7 +95,7 @@ require_once('metroLib.php');
 $ml = NXLogisMetro\metroLib::getInstance();
 
 // 역별 열차 운행정보를 조회합니다. (예시: 1호선 구로역)
-$trainList = ml->getDataByStation('1', '1701');
+$trainList = ml->getDataByStation('1701')
 
 // 화면에 데이터를 표시합니다.
 var_dump($trainList);
@@ -107,11 +103,9 @@ var_dump($trainList);
 
 ## 라이선스
 GNU Affero GPL v3   
-특정인 또는 단체가 정보를 독점하는 일을 막기 위해 불가피하게 적용되었습니다. 꼭 준수하여 주시기 바랍니다.   
 
 ## 그 외
 * metroLib를 통해 얻은 정보는 정확하지 않을 수 있습니다. 
-* 일부 노선 구간(특히 1, 3, 4호선 공용구간) 내의 열차 편성정보 및 시종착역 정보가 제공되지 않을 수 있습니다.
-* 한국철도공사의 비공개 API(엄밀히는 코레일지하철톡만을 위한 API)를 이용한 것이므로 사용상의 책임은 사용자가 모두 부담하며, 코드 저작자 및 운영기관에서는 어떠한 경우에도 책임지지 않습니다.   
-* metroLib 및 metroLib를 통해 얻은 데이터를 악용하거나 한국철도공사(KORAIL) 또는 서울교통공사(SeoulMetro)에 민원을 제기하지 마세요. 서비스 중단으로 이어질 수 있습니다.   
-* 문의, 버그 제보는 Issues 기능을 이용하셔서 등록하면 됩니다. 누구든지 환영합니다.
+* 일부 구간(노선)에서는 편성정보 및 시·종착역 정보가 제공되지 않을 수 있습니다.
+* 서울교통공사의 비공개 API(엄밀히는 또타지하철 앱 전용 API)를 이용한 것이므로 사용상의 책임은 사용자가 모두 부담합니다.
+* metroLib 및 metroLib를 통해 얻은 데이터를 악용하거나 한국철도공사(KORAIL) 또는 서울교통공사(SeoulMetro)에 민원을 제기하지 마세요.
--- metroLib.php
+++ metroLib.php
@@ -7,28 +7,23 @@ class metroLib {
 	* 수도권 전철 1호선, 3호선 및 4호선의 경우 행선지, 급행여부가 누락되거나 오기되는 경우가 있어 이를 TOPIS API를 이용하여 보정할 수 있습니다.
 	* 이 기능을 사용하지 않는다면 false로 설정하십시오.
 	*/
-	private const USE_TOPIS_API = true;
+	protected const USE_TOPIS_API = true;
 
 	/*
 	* TOPIS API 주소를 API 키와 함께 적어주십시오.
 	* USE_TOPIS_API 상수가 false로 지정되었을 경우 사용되지 않습니다.
 	* 형식은 반드시 'http://swopenapi.seoul.go.kr/api/subway/[YOUR API KEY]/json/realtimePosition/0/80/' 여야 합니다.
 	*/
-	private const TOPIS_URL = 'http://swopenapi.seoul.go.kr/api/subway//json/realtimePosition/0/80/';
+	protected const TOPIS_URL = 'http://swopenapi.seoul.go.kr/api/subway//json/realtimePosition/0/80/';
 
 	/*
 	* 여기서부터는 건드리지 마십시오.
 	*/
-	private const PACKET_TYPE_LINE = 0;
-	private const PACKET_TYPE_STN = 1;
+	protected const GET_TYPE_LINE = 0;
+	protected const GET_TYPE_STN = 1;
 
-	private const address = 'tcp://minwon.korail.com:20301';
-	private const cipher = 'aes-128-ecb';
-	private const key = '1231231230123123';
-
-	private static $instance = null;
-	private $socket = null;
-	private $line_data = null;
+	protected static $instance = null;
+	protected $line_data = null;
 
 	/*
 	* metroLib 인스턴스를 가져옵니다.
@@ -48,12 +43,12 @@ class metroLib {
 	* 기준 역 ±4역 범위 내의 열차 운행정보만을 가져올 수 있습니다.
 	*
 	* @access public
-	* @param string $line_code, string $station_code
+	* @param string $station_code
 	* @return ?array
 	* @throw \RuntimeException
 	*/
-	public function getDataByStation(string $line_code, string $station_code) : ?array {
-		$train_list = $this->getDataFromServer($line_code, $station_code);
+	public function getDataByStation(string $station_code) : ?array {
+		$train_list = $this->getDataFromServer(self::GET_TYPE_STN, $station_code);
 		return $train_list;
 	}
 
@@ -66,116 +61,72 @@ class metroLib {
 	* @throw \RuntimeException
 	*/
 	public function getDataByLine(string $line_code) : ?array {
-		$trn_dirs = null;
-		if ($line_code == '2')
-			$trn_dirs = ['1', '2', '3', '4', '5', '6'];
-		else
-			$trn_dirs = ['1', '2'];
-
-		$train_list = array();
-		foreach ($trn_dirs as $trn_dir) {
-			$res = $this->getDataFromServer($line_code, null, $trn_dir);
-			if ($res != null)
-				$train_list = array_merge($train_list, $res);
-		}
-		$train_list = count($train_list) > 0 ? $train_list : null;
-
+		$train_list = $this->getDataFromServer(self::GET_TYPE_LINE, $line_code);
 		return $train_list;
 	}
 
 	/*
 	* 서버에서 데이터를 갖고와서 처리 후 반환합니다.
 	*
-	* @access private
-	* @param string $line_code, ?string $station_code = null, ?string $trn_dir = null
+	* @access protected
+	* @param int $type
+	* @param string $param
 	* @return ?array
 	* @throw \RuntimeException
 	*/
-	private function getDataFromServer(string $line_code, ?string $station_code = null, ?string $trn_dir = null) {
-		$req = null;
-		if ($station_code == null) {
-			$req = "line={$line_code}&gbn=0&dir={$trn_dir}&";
-			$req = $this->makePacket($req, self::PACKET_TYPE_LINE);
-		} else {
-			$req = "lineGbn=0&stationCd={$station_code}&lineCd={$line_code}&";
-			$req = $this->makePacket($req, self::PACKET_TYPE_STN);
+	protected function getDataFromServer(int $type, string $param) : ?array {
+		$url = '';
+		switch ($type) {
+			case self::GET_TYPE_LINE:
+				$url = 'https://smss.seoulmetro.co.kr/api/3010.do';
+				$param = "lineNumCd={$param}";
+				break;
+			case self::GET_TYPE_STN:
+				$url = 'https://smss.seoulmetro.co.kr/api/13000.do';
+				$param = "stationCd={$param}";
+				break;
+			default:
+				throw \InvalidArgumentException('Argument $type is invalid. value='.$type);
 		}
 
-		$res = fwrite($this->socket, $req);
-		if ($res === false)
-			throw new \RuntimeException('Socket writing error.');
+		$ch = curl_init();
 
-		usleep(100000 * 3); // Wait 0.3s
+		curl_setopt($ch, CURLOPT_POST, true);
+		curl_setopt($ch, CURLOPT_URL, $url);
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+		curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+		curl_setopt($ch, CURLOPT_POSTFIELDS, $param);
 
-		$res = stream_get_contents($this->socket);
-		if ($res === false)
-			throw new \RuntimeException('Socket reading error.');
-
-		if (strlen($res) < 7)
-			throw new \RuntimeException('Received data is invalid.');
-
-		$res = $this->decryptData($res);
-		if (empty($res))
-			throw new \RuntimeException('Response is empty.');
+		$res = curl_exec($ch);
+		curl_close($ch);
 
 		$res = json_decode($res, true);
 		$err = json_last_error();
 		if ($err != JSON_ERROR_NONE)
-			throw new \RuntimeException('JSON parse error.');
+			throw new \RuntimeException('Json parse error.');
 
 		if (!$res['isValid'])
 			throw new \RuntimeException('Remote server error.');
 
-		$res = $res['trainVOList'];
+		$res = $res['ttcVOList'];
 		if (count($res) == 0)
 			return null;
 
-		$train_list = $this->processTrainList($res, $line_code, $station_code);
+		$train_list = $this->processTrainList($res);
 		return $train_list;
 	}
 
 	/*
 	* 서버에서 받아온 데이터를 처리합니다.
 	*
-	* @access private
-	* @param array $par, string $line_code, ?string $station_code = null
+	* @access protected
+	* @param array $par
 	* @return ?array
 	* @throw \RuntimeException, \InvalidArgumentException
 	*/
-	private function processTrainList(array $par, string $line_code, ?string $station_code = null) : array {
-		$station_name = null;
-		if ($station_code != null) {
-			foreach ($this->line_data[$line_code] as $stnNm => $stnCd) {
-				if ($stnCd === $station_code) {
-					$station_name = $stnNm;
-					break;
-				}
-			}
-
-			if ($station_name == null)
-				throw new \InvalidArgumentException('$station_code is invalid., '.$station_code);
-		}
-
+	protected function processTrainList(array $par) : array {
 		$train_list = array();
 		foreach ($par as $e) {
-			// for duplicated train
-			$is_duplicated = false;
-			for ($i = 0; $i < count($train_list); $i++) {
-				if ($train_list[$i]['trn_no'] == $e['trainY']) {
-					$train_list[$i]['stn_nm'] = isset($e['stnNm']) ? $e['stnNm'] : $station_name;
-					
-					$train_list[$i]['trn_sts'] = $e['sts'];
-					if (isset($e['orgStnNm']) && isset($e['stnNm']) && $e['orgStnNm'] == $e['stnNm'] && $e['sts'] == 2)
-						$train_list[$i]['trn_sts'] = 0;
-
-					$is_duplicated = true;
-					break;
-				}
-			}
-
-			if ($is_duplicated)
-				continue;
-
 			$is_line2 = strval($e['line']) == '2';
 			$trn_dirs = $is_line2 ? ['I', 'O', 'SI1', 'SO1', 'SI2', 'SO2'] : ['U', 'D'];
 
@@ -183,22 +134,31 @@ class metroLib {
 			$train['trn_no'] = ($is_line2 && substr($e['trainY'], 0, 1) != 'S' ? 'S' : '').$e['trainY'];
 			$train['line'] = strval($e['line']);
 
+			// 3, 4호선 열차번호가 잘못된 경우를 수정
+			if ($train['line'] == '3' && substr($train['trn_no'], 1, 1) != '3')
+				$train['trn_no'] = substr($train['trn_no'], 0, 1).'3'.substr($train['trn_no'], 2);
+			else if ($train['line'] == '4' && substr($train['trn_no'], 1, 1) != '4')
+				$train['trn_no'] = substr($train['trn_no'], 0, 1).'4'.substr($train['trn_no'], 2);
+
 			$train['trn_form_no'] = $e['trainP'] ?? null;
-			if ($train['trn_form_no'] == '000' || $train['trn_form_no'] == '0000')
+			// 000이면 편성정보가 없다는 의미임.
+			if ($train['trn_form_no'] == '000')
 				$train['trn_form_no'] = null;
-			if ($train['trn_form_no'] != null)
-				$train['trn_form_no'] = ltrim($train['trn_form_no'], '0');
 
 			$train['trn_sts'] = $e['sts'];
 			if (isset($e['orgStnNm']) && isset($e['stnNm']) && $e['orgStnNm'] == $e['stnNm'] && $e['sts'] == 2)
 				$train['trn_sts'] = 0;
-			else if (isset($e['orgStnNm']) && $e['orgStnNm'] == $station_name && $e['sts'] == 2)
-				$train['trn_sts'] = 0;
 
-			$train['is_exp'] = isset($e['express']) ? $e['express'] === 'Y' : false;
+			$train['is_exp'] = $e['directAt'] === '1';
 
-			$train['stn_nm'] = isset($e['stnNm']) ? $this->removeSubStnNm($e['stnNm']) : null;
-			$train['stn_cd'] = isset($e['stnCd']) ? $e['stnCd'] : null;
+			$train['stn_nm'] = isset($e['stationNm']) ? $this->removeSubStnNm($e['stationNm']) : null;
+			switch ($train['stn_nm']) {
+				case '서울':
+					$train['stn_nm'] = '서울역';
+					break;
+				default:
+			}
+			$train['stn_cd'] = $train['stn_nm'] == null ? null : $this->line_data[$train['line']][$train['stn_nm']];
 
 			if (isset($e['dir']))
 				$train['trn_dir'] = $trn_dirs[$e['dir'] - 1];
@@ -206,33 +166,31 @@ class metroLib {
 				$train['trn_dir'] = null;
 
 			$train['org_stn_nm'] = isset($e['orgStnNm']) && $e['orgStnNm'] != '0' ? $this->removeSubStnNm($e['orgStnNm']) : null;
-			$train['org_stn_cd'] = isset($e['orgStn']) && $e['orgStn'] != '0' ? $e['orgStn'] : null;
-			if (strlen($train['org_stn_cd']) != 4)
-				$train['org_stn_cd'] = null;
+			$train['org_stn_cd'] = $train['org_stn_nm'] == null ? null : $this->line_data[$train['line']][$train['org_stn_nm']];
 
-			if ($is_line2 && ($e['dstStn'] == '88_1' || $e['dstStn'] == '88_2')) { // 내·외선순환 관련 처리
-				$train['dst_stn_nm'] = $train['org_stn_nm'];
-				$train['dst_stn_cd'] = $train['org_stn_cd'];
-			} else {
+			if (isset($e['dstStn'])) {
 				$train['dst_stn_nm'] = isset($e['dstStnNm']) && $e['dstStnNm'] != '0' ? $this->removeSubStnNm($e['dstStnNm']) : null;
-				$train['dst_stn_cd'] = isset($e['dstStn']) && $e['dstStn'] != '0' ? $e['dstStn'] : null;
+				$train['dst_stn_cd'] = $train['dst_stn_nm'] == null ? null : $this->line_data[$train['line']][$train['dst_stn_nm']];
+			} else {
+				$train['dst_stn_nm'] = isset($e['statnTnm']) && $e['statnTnm'] != '0' ? $this->removeSubStnNm($e['statnTnm']) : null;
+				
+				// 2호선 외·내선순환행은 성수 시종착임.
+				if ($is_line2 && ($train['dst_stn_nm'] == '외선순환' || $train['dst_stn_nm'] == '내선순환')) 
+					$train['dst_stn_nm'] = '성수'; 
+
+				$train['dst_stn_cd'] = $train['dst_stn_nm'] == null ? null : $this->line_data[$train['line']][$train['dst_stn_nm']];					
 			}
 
 			// 종착처리
 			if (isset($train['dst_stn_nm']) && $train['dst_stn_nm'] == $train['stn_nm'] && $train['trn_sts'] == 2)
-				$train['trn_sts'] = 4;
-
-			$train['prev_stn_nm'] = isset($e['prevStnNm']) ? $this->removeSubStnNm($e['prevStnNm']) : (isset($e['startStnNm']) ? $this->removeSubStnNm($e['startStnNm']) : null);
-			$train['prev_stn_nm'] = isset($e['prevStnCd']) ? $this->removeSubStnNm($e['prevStnCd']) : (isset($e['startStnCd']) ? $e['startStnCd'] : null);
-			$train['next_stn_nm'] = isset($e['nextStnNm']) ? $this->removeSubStnNm($e['nextStnNm']) : (isset($e['endStnNm']) ? $this->removeSubStnNm($e['endStnNm']) : null);
-			$train['next_stn_nm'] = isset($e['nextStnCd']) ? $this->removeSubStnNm($e['nextStnCd']) : (isset($e['endStnCd']) ? $e['endStnCd'] : null);
+				$train['trn_sts'] = 5;
 
 			$train_list[] = $train;
 		}
 
 		// TOPIS API를 이용하여 급행여부, 위치, 상태, 행선지 보정
-		if (self::USE_TOPIS_API) {
-			$train_list2 = $this->getDataByTopis($line_code);
+		if (self::USE_TOPIS_API && count($train_list) > 0) {
+			$train_list2 = $this->getDataByTopis($train_list[0]['line']);
 			if ($train_list2 != null) {
 				for ($i = 0; $i < count($train_list); $i++) {
 					$trn_no = substr($train_list[$i]['trn_no'], 1);
@@ -250,17 +208,13 @@ class metroLib {
 			}
 		}
 
-		// 행선지 보정
+		// 처리되지 않은 행선지 보정
 		for ($i = 0; $i < count($train_list); $i++) {
 			if ($train_list[$i]['line'] == '1') {
 				switch ($train_list[$i]['dst_stn_nm']) {
 					case '서울': // 1호선 녹색급행 서울역행 관련 처리
 						$train_list[$i]['dst_stn_nm'] = '서울역';
 						$train_list[$i]['dst_stn_cd'] = $this->line_data['1']['서울역'];
-						break;		
-					case '종로3가': // 광명셔틀 영등포행이 종로3가행으로 나오는 문제가 있음.
-						$train_list[$i]['dst_stn_nm'] = '영등포';
-						$train_list[$i]['dst_stn_cd'] = $this->line_data['1']['영등포'];
 						break;		
 					case '수원': // 서동탄행이 수원행으로 나오는 문제가 있음.
 						$train_list[$i]['dst_stn_nm'] = '서동탄';
@@ -303,12 +257,12 @@ class metroLib {
 	/*
 	* TOPIS API 서버로부터 열차 정보를 갖고옵니다. (1, 3, 4호선 보정용)
 	*
-	* @access private
+	* @access protected
 	* @param string $line_code
 	* @return ?array
 	* @throw \RuntimeException
 	*/
-	private function getDataByTopis(string $line_code) : ?array {
+	protected function getDataByTopis(string $line_code) : ?array {
 		$line_list = ['1' => '1호선', '3' => '3호선', '4' => '4호선'];
 
 		if (!array_key_exists($line_code, $line_list))
@@ -368,125 +322,49 @@ class metroLib {
 		return $train_list;
 	}
 
-	/*
-	* 요청 값을 형식에 맞게 암호화합니다.
-	*
-	* @access private
-	* @param string $req, int $packet_type
-	* @return string
-	* @throw \RuntimeException, \InvalidArgumentException
-	*/
-	private function makePacket(string $req, int $packet_type) : string {
-		// Block Size Padding
-		$req_size = strlen($req);
-		if ($req_size % 16 != 0)
-			$req = str_pad($req, $req_size + 16 - $req_size % 16, "\0");
-
-		// Encryption
-		$req = openssl_encrypt($req, self::cipher, self::key, OPENSSL_RAW_DATA | OPENSSL_NO_PADDING);
-		if ($req === false)
-			throw new \RuntimeException('Encryption failed.');
-
-		/*
-		* Packet Structure
-		*
-		* Header (Total 7 bytes)
-		* TR_CODE(2 bytes) : {0x0B, 0xC2} or {0x42, 0x72}
-		* IS_ENCRYPTED(1 byte) : {0x1}
-		* RETURN_CODE(2 bytes) : Request packet must be set to {0x0, 0x0}
-		* DATA_LENGTH(2 bytes) : Body Length(UTF-8, encrypted)
-		* -----
-		* BODY(Variable) : The data must be encrypted with AES-128-ECB-NoPadding before send to the server.
-		*/
-
-		$req_size = strlen($req);
-
-		$req = unpack('C*', $req);
-
-		$req_data = array();
-		// TR_CODE
-		if ($packet_type == self::PACKET_TYPE_LINE) {
-			$req_data[0] = 0x42;
-			$req_data[1] = 0x72;
-		} else if ($packet_type == self::PACKET_TYPE_STN) {
-			$req_data[0] = 0x0B;
-			$req_data[1] = 0xC2;
-		} else
-			throw new \InvalidArgumentException('$packet_type is invalid.');
-
-		// IS_ENCRYPTED
-		$req_data[2] = 0x1;
-		// RET_CODE
-		$req_data[3] = 0x0;
-		$req_data[4] = 0x0;
-		// DATA_LENGTH
-		if ($req_size > 255) {
-			$req_data[5] = dechex(intval($req_size / 256));
-			$req_data[6] = dechex($req_size % 256);
-		} else {
-			$req_data[5] = 0x0;
-			$req_data[6] = intval($req_size);
-		}
-		$req_data = array_merge($req_data, $req);
-		$req_data = vsprintf(str_repeat('%c', count($req_data)), $req_data);
-
-		return $req_data;
-	}
-
-	/*
-	* 응답 메시지를 복호화합니다.
-	*
-	* @access private
-	* @param string $stn_nm
-	* @return string
-	* @throw RuntimeErrorException
-	*/
-	private function decryptData(string $res) : string {
-		$res = substr($res, 7); // Get response body only.
-		
-		$ret = openssl_decrypt($res, self::cipher, self::key, OPENSSL_RAW_DATA | OPENSSL_NO_PADDING);
-		if ($ret === false)
-			throw new \RuntimeException('Decryption failed.');
-
-		$ret = trim($ret);
-		return $ret;
-	}
 
 	/*
 	* 부기역명을 제거합니다.
 	*
-	* @access private
+	* @access protected
 	* @param string $stn_nm
 	* @return string
 	*/
-	private function removeSubStnNm(string $stn_nm) : string {
+	protected function removeSubStnNm(string $stn_nm) : string {
 		return preg_replace('/\([0-9가-힣A-z]+\)$/u', '', $stn_nm);
+	}
+
+	/*
+	* Json 파일에서 데이터를 불러와 Array로 반환합니다.
+	*
+	* @access protected
+	* @param string $file_path
+	* @return ?array
+	*/
+	protected function readArrayFromJsonFile(string $file_path) : ?array {
+		if (!file_exists($file_path))
+			throw new \RuntimeException('file_path is not exist., value='.$filePath);
+
+		$fp = fopen($file_path, 'r');
+		$data = fread($fp, filesize($file_path));
+		fclose($fp);
+
+		$data = json_decode($data, true);
+		$err = json_last_error();
+		if ($err != JSON_ERROR_NONE)
+			throw new \RuntimeException('Json parse error.');
+
+		return $data;
 	}
 
 	/*
 	* 생성자. new metroLib()를 쓰지 마시고 metroLib::getInstance()를 써주세요.
 	*
-	* @access private
+	* @access protected
 	*/
-	private function __construct() {
+	protected function __construct() {
 		$line_data_filename = __DIR__.'/stations.json';
-		if (!file_exists($line_data_filename))
-			throw new \RuntimeException('stations.json is not exist.');
-
-		$line_data = file_get_contents($line_data_filename);
-		$line_data = json_decode($line_data, true);
-		$err = json_last_error();
-		if ($err != JSON_ERROR_NONE)
-			throw new \RuntimeException('stations.json parse error.');
-		$this->line_data = $line_data;
-
-		$error_no = null;
-		$error_str = null;
-		$this->socket = stream_socket_client(self::address, $error_no, $error_str, 0.5);
-		if ($this->socket === false)
-			throw new \RuntimeException("Connection error: {$error_no}: {$error_str}");
-
-		stream_set_blocking($this->socket, false);
+		$this->line_data = $this->readArrayFromJsonFile($line_data_filename);
 	}
 }
 ?>
